3c0d715dac8a1ca8ed6ac7102c27fc3506ca245d
kswang <wangkaisheng2@huawei.com>
2022-10-11 10:47:33 +0800

    mv bert finetune file

--- transformer/processor/assessment_method.py
+++ transformer/processor/assessment_method.py
@@ -19,7 +19,7 @@ Bert evaluation assessment method script.
 import math
 import numpy as np
 from mindspore.nn.metrics import ConfusionMatrixMetric
-from .CRF import postprocess
+from transformer.processor.CRF import postprocess
 
 class Accuracy():
     '''
