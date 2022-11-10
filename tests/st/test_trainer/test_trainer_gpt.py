92e3dadd6884ac8be293bdce03d80f56f637e381
jiahongQian <qianjiahong@huawei.com>
2022-11-10 18:58:38 +0800

    fix tests

--- tests/st/test_trainer/test_trainer_gpt.py
+++ tests/st/test_trainer/test_trainer_gpt.py
@@ -62,7 +62,6 @@ def test_trainer_gpt_train():
     trainer.train()
 
 @pytest.mark.level0
-@pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.env_onecard
 def test_trainer_gpt_by_cmd():
