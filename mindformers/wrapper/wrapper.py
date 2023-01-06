622100fee0498bb298995874b138ffa74f749d21
AHHOZP <137954843@qq.com>
2023-01-06 16:31:25 +0800

    Add Wrapper

--- mindformers/wrapper/wrapper.py
+++ mindformers/wrapper/wrapper.py
@@ -14,12 +14,16 @@
 # ============================================================================
 """Self-Define Wrapper."""
 from mindspore import nn
+from mindspore.common import RowTensor
+from mindspore.ops import composite as C
+from mindspore.ops import functional as F
 from mindspore.ops import operations as P
+
 
 from mindformers.tools.register import MindFormerRegister, MindFormerModuleType
 
 
-__all__ = ['ClassificationMoeWrapper']
+__all__ = ['ClassificationMoeWrapper', 'TrainOneStepWithClipGN']
 
 
 @MindFormerRegister.register(MindFormerModuleType.WRAPPER)
@@ -45,3 +49,57 @@ class ClassificationMoeWrapper(nn.WithLossCell):
             Cell, the backbone network.
         """
         return self._backbone
+
+
+_grad_scale = C.MultitypeFuncGraph("grad_scale")
+reciprocal = P.Reciprocal()
+
+
+@_grad_scale.register("Tensor", "Tensor")
+def tensor_grad_scale(scale, grad):
+    return grad * F.cast(reciprocal(scale), F.dtype(grad))
+
+
+@_grad_scale.register("Tensor", "RowTensor")
+def tensor_grad_scale_row_tensor(scale, grad):
+    return RowTensor(grad.indices,
+                     grad.values * F.cast(reciprocal(scale), F.dtype(grad.values)),
+                     grad.dense_shape)
+
+
+@MindFormerRegister.register(MindFormerModuleType.WRAPPER)
+class TrainOneStepWithClipGN(nn.TrainOneStepWithLossScaleCell):
+    """TrainOneStep"""
+
+    def __init__(self, network, optimizer,
+                 use_clip_grad=False, clip_norm=1.0,
+                 scale_sense=1.0):
+        super(TrainOneStepWithClipGN, self).__init__(network, optimizer, scale_sense)
+        self.print = P.Print()
+        self.clip_norm = clip_norm
+        self.use_clip_grad = use_clip_grad
+
+    def construct(self, *inputs):
+        """construct"""
+        weights = self.weights
+        loss = self.network(*inputs)
+        scaling_sens = self.scale_sense
+
+        status, scaling_sens = self.start_overflow_check(loss, scaling_sens)
+
+        scaling_sens_filled = C.ones_like(loss) * F.cast(scaling_sens, F.dtype(loss))
+        grads = self.grad(self.network, weights)(*inputs, scaling_sens_filled)
+        grads = self.hyper_map(F.partial(_grad_scale, scaling_sens), grads)
+        # apply grad reducer on grads
+        grads = self.grad_reducer(grads)
+        # get the overflow buffer
+        cond = self.get_overflow_status(status, grads)
+        overflow = self.process_loss_scale(cond)
+        # if there is no overflow, do optimize
+        if not overflow:
+            if self.use_clip_grad:
+                grads = C.clip_by_global_norm(grads, clip_norm=self.clip_norm)
+            loss = F.depend(loss, self.optimizer(grads))
+        else:
+            self.print("==========Overflow Now============")
+        return loss
