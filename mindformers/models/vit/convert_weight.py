ef207f9b7f57a8c3f6286afbb32d367dcfe3882c
mookie <zhanghaoyong1@huawei.com>
2023-02-10 11:05:55 +0800

    修改MAE/ViT权重转换脚本，支持large和huge

--- mindformers/models/vit/convert_weight.py
+++ mindformers/models/vit/convert_weight.py
@@ -14,18 +14,20 @@
 # ============================================================================
 """Convert checkpoint from torch/facebook"""
 import argparse
-import mindspore as ms
 import torch
+import mindspore as ms
+from mindformers.tools import logger
 
 
-def convert_weight(pth_file="mae_pretrain_vit_base.pth", ms_ckpt_path="mae_pretrain_vit_base.ckpt"):
+def convert_weight(torch_pth_path="mae_pretrain_vit_base.pth", ms_ckpt_path="mae_pretrain_vit_base.ckpt"):
     """
     convert mae_vit_base_p16 weights from pytorch to mindspore
     pytorch and GPU required.
     """
-    ms_ckpt = []
-    state_dict = torch.load(pth_file, map_location=torch.device('cpu'))
-    for k, v in state_dict['model'].items():
+    param_dict = torch.load(torch_pth_path, map_location=torch.device("cpu"))
+
+    new_dict = []
+    for k, v in param_dict["model"].items():
         if k in ("cls_token", "mask_token"):
             k += "s"
         if "norm" in k:
@@ -51,42 +53,32 @@ def convert_weight(pth_file="mae_pretrain_vit_base.pth", ms_ckpt_path="mae_pretr
                 k = k.replace("proj", "projection")
                 if "weight" in k:
                     v = v.transpose(-1, 0)
-        if 'qkv' not in k:
-            ms_ckpt.append({'name': k, 'data': ms.Tensor(v.numpy())})
-            print(k)
+        if "qkv" not in k:
+            new_dict.append({"name": k, "data": ms.Tensor(v.numpy())})
         else:
-            if k.startswith("decoder"):
-                data = ms.Tensor(v.numpy())
-                ms_ckpt.append({'name': k.replace('.qkv', '.dense1'), 'data': data[:512]})
-                ms_ckpt.append({'name': k.replace('.qkv', '.dense2'), 'data': data[512:1024]})
-                ms_ckpt.append({'name': k.replace('.qkv', '.dense3'), 'data': data[1024:]})
-                print(k.replace('.qkv', '.dense1'))
-                print(k.replace('.qkv', '.dense2'))
-                print(k.replace('.qkv', '.dense3'))
-            else:
-                data = ms.Tensor(v.numpy())
-                ms_ckpt.append({'name': k.replace('.qkv', '.dense1'), 'data': data[:768]})
-                ms_ckpt.append({'name': k.replace('.qkv', '.dense2'), 'data': data[768:1536]})
-                ms_ckpt.append({'name': k.replace('.qkv', '.dense3'), 'data': data[1536:]})
-                print(k.replace('.qkv', '.dense1'))
-                print(k.replace('.qkv', '.dense2'))
-                print(k.replace('.qkv', '.dense3'))
+            data = ms.Tensor(v.numpy())
+            length = data.shape[0] // 3
+            new_dict.append({"name": k.replace(".qkv", ".dense1"), "data": data[:length]})
+            new_dict.append({"name": k.replace(".qkv", ".dense2"), "data": data[length:length*2]})
+            new_dict.append({"name": k.replace(".qkv", ".dense3"), "data": data[length*2:]})
 
-    ms.save_checkpoint(ms_ckpt, ms_ckpt_path)
+    ms.save_checkpoint(new_dict, ms_ckpt_path)
+
+    logger.info("Weights conversion completes. ")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="mae vit weight convert script")
-    parser.add_argument("--torch_path",
+    parser.add_argument("--torch_pth_path",
                         type=str,
                         default="mae_pretrain_vit_base.pth",
                         required=True,
                         help="The torch checkpoint path.")
-    parser.add_argument("--mindspore_path",
+    parser.add_argument("--ms_ckpt_path",
                         type=str,
                         required=True,
                         default="mae_pretrain_vit_base.ckpt",
                         help="The output mindspore checkpoint path.")
     opt = parser.parse_args()
 
-    convert_weight(opt.torch_path, opt.mindspore_path)
+    convert_weight(opt.torch_pth_path, opt.ms_ckpt_path)
