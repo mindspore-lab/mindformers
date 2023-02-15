ca37076d96cc4495ba632eb5bd4a47cdd7613862
zyw_hw <zhangyouwen7@huawei.com>
2023-02-15 19:47:14 +0800

    add gpt optim

--- mindformers/common/optim/__init__.py
+++ mindformers/common/optim/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Huawei Technologies Co., Ltd
+# Copyright 2023 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
 # ============================================================================
 """MindFormers Optimizer."""
 from .build_optim import build_optim
-
+from .optim import *
 
 __all__ = ['build_optim']
+__all__.extend(optim.__all__)
