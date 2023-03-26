abd5127159ef48a5eff15204adc364cacfa489c6
zyw-hw <zhangyouwen7@huawei.com>
2023-03-26 11:52:00 +0800

    gpt2 pipeline

--- mindformers/trainer/causal_language_modeling/causal_language_modeling.py
+++ mindformers/trainer/causal_language_modeling/causal_language_modeling.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 # ============================================================================
 """Causal Image Modeling Trainer."""
+import os
 from typing import Optional, List, Union
 
 from mindspore.train import Callback
@@ -20,10 +21,12 @@ from mindspore.nn import TrainOneStepCell, Optimizer, Cell
 from mindspore.dataset import GeneratorDataset
 
 from mindformers.dataset import BaseDataset
-from mindformers.models import BaseModel
+from mindformers.models import build_model, BaseModel, BaseTokenizer, build_tokenizer
+from mindformers.tools.logger import logger
+from mindformers.tools.utils import count_params
+from mindformers.pipeline import pipeline
+from mindformers.tools.register import MindFormerRegister, MindFormerModuleType, MindFormerConfig
 
-from mindformers.tools.register import MindFormerRegister, \
-    MindFormerModuleType, MindFormerConfig
 from ..config_args import ConfigArguments
 from ..training_args import TrainingArguments
 from ..base_trainer import BaseTrainer
@@ -95,6 +98,82 @@ class CausalLanguageModelingTrainer(BaseTrainer):
         raise NotImplementedError(
             "The CausalLanguageModeling task does not support evaluate.")
 
-    def predict(self, *args, **kwargs):
-        raise NotImplementedError(
-            "The CausalLanguageModeling task does not support predict.")
+    def predict(self,
+                config: Optional[Union[dict, MindFormerConfig, ConfigArguments, TrainingArguments]] = None,
+                input_data: Optional[Union[str, list, GeneratorDataset]] = None,
+                network: Optional[Union[Cell, BaseModel]] = None,
+                tokenizer: Optional[BaseTokenizer] = None,
+                **kwargs):
+        """
+        Executes the predict of the trainer.
+
+        Args:
+            config (Optional[Union[dict, MindFormerConfig, ConfigArguments, TrainingArguments]]):
+                The task config which is used to configure the dataset, the hyper-parameter, optimizer, etc.
+                It supports config dict or MindFormerConfig or TrainingArguments or ConfigArguments class.
+                Default: None.
+            input_data (Optional[Union[Tensor, str, list]]): The predict data. It supports 1) a text string to be
+                translated, 1) a file name where each line is a text to be translated  and 3) a generator dataset.
+                Default: None.
+            network (Optional[Union[Cell, BaseModel]]): The network for trainer.
+                It supports model name or BaseModel or MindSpore Cell class.
+                Default: None.
+            tokenizer (Optional[BaseTokenizer]): The tokenizer for tokenizing the input text.
+                Default: None.
+        Returns:
+            A list of prediction.
+
+        """
+        config = self.set_config(config)
+
+        if input_data is None:
+            input_data = config.input_data
+
+        if not isinstance(input_data, (str, list, GeneratorDataset)):
+            raise ValueError("Input data's type must be one of "
+                             f"[str, list, GeneratorDataset], but got type {type(input_data)}")
+
+        logger.info(".........Build Net..........")
+
+        if tokenizer is None:
+            tokenizer = build_tokenizer(config.processor.tokenizer)
+
+        if network is None:
+            network = build_model(config.model)
+
+        if network is not None:
+            logger.info("Network Parameters: %s M.", str(count_params(network)))
+
+        save_file = kwargs.pop("save_file", None)
+        if save_file is None:
+            if config and config.save_file is not None:
+                save_file = config.save_file
+            else:
+                save_file = "results.txt"
+
+        pipeline_task = pipeline(task='text_generation',
+                                 tokenizer=tokenizer,
+                                 model=network,
+                                 max_length=network.config.seq_length,
+                                 **kwargs
+                                 )
+        if isinstance(input_data, str) and os.path.isfile(input_data):
+            with open(input_data, 'r') as fp:
+                output_result = []
+                for line in fp:
+                    output = pipeline_task(line, **kwargs)
+                    output_result.extend(output)
+        else:
+            output_result = pipeline_task(input_data, **kwargs)
+
+        logger.info(".........start to write the output result to: %s.........", save_file)
+        with open(save_file, 'w') as file:
+            if isinstance(output_result, list):
+                for item in output_result:
+                    file.write(str(item) + '\n')
+            else:
+                file.write(str(output_result))
+            file.close()
+        logger.info(".........writing result finished..........")
+        logger.info(".........Predict Over!.............")
+        return output_result
