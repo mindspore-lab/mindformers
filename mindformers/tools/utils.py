af4112a4af86d008a036c90110d4b22a5dcc1d33
linyinglai <linyinglai@huawei.com>
2023-03-28 22:06:33 +0800

    "修正 mindspore 版本判断问题"

--- mindformers/tools/utils.py
+++ mindformers/tools/utils.py
@@ -249,16 +249,16 @@ def try_sync_file(file_name):
 
 def is_version_le(current_version, base_version):
     """
-        return current_version >= base_version.
-        Check whether the current version is higher than or equal to the base version.
-        For example: base_version: 0.1.2, current_version 0.3.1, will return True.
+        return current_version <= base_version.
+        Check whether the current version is lower than or equal to the base version.
+        For example: for current_version: 0.3.1, base_version: 0.1.2, it return False.
     """
     version_split_char = '.'
     if version_split_char not in base_version or version_split_char not in current_version:
         raise ValueError("The version string will contain the `.`."
-                         "For example, base_version: 0.1.2, current_version 0.3.1.")
+                         "For example, current_version 0.3.1, base_version: 0.1.2.")
     for x, y in zip(current_version.split(version_split_char), base_version.split(version_split_char)):
-        if int(x) < int(y):
+        if int(x) > int(y):
             return False
     return True
 
@@ -267,13 +267,13 @@ def is_version_ge(current_version, base_version):
     """
         return current_version >= base_version.
         Check whether the current version is higher than or equal to the base version.
-        For example: base_version: 0.1.2, current_version 0.3.1, will return True.
+        For example: current_version 0.3.1, base_version: 0.1.2, it return True.
     """
     version_split_char = '.'
     if version_split_char not in base_version or version_split_char not in current_version:
         raise ValueError("The version string will contain the `.`."
-                         "For example, base_version: 0.1.2, current_version 0.3.1.")
+                         "For example, current_version 0.3.1， base_version: 0.1.2.")
     for x, y in zip(current_version.split(version_split_char), base_version.split(version_split_char)):
-        if int(x) > int(y):
+        if int(x) < int(y):
             return False
     return True
