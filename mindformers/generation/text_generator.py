1b84422a1209c3331461e64587c6701916cd46cf
huanglei_Sorry <huanglei_zju@163.com>
2023-08-10 17:07:19 +0800

    refactor sample in text generator

--- mindformers/generation/text_generator.py
+++ mindformers/generation/text_generator.py
@@ -27,81 +27,15 @@ from mindspore.common.tensor import Tensor
 from mindformers.generation.streamers import BaseStreamer
 
 from .generation_config import GenerationConfig
+from .logits_process import (LogitNormalization, LogitsProcessorList,
+                             RepetitionPenaltyLogitsProcessor,
+                             TemperatureLogitsWarper, TopKLogitsWarper,
+                             TopPLogitsWarper)
 from .streamers import BaseStreamer
 from .utils import softmax
 from ..tools import logger
 
 __all__ = ["GeneratorMixin"]
-
-
-def topk_fun(logits, topk=5):
-    """Get topk"""
-    batch_value = []
-    batch_index = []
-    for i in range(logits.shape[0]):
-        target_column = logits[i].tolist()
-        sorted_array = [(k, v) for k, v in enumerate(target_column)]
-        sorted_array.sort(key=lambda x: x[1], reverse=True)
-        topk_array = sorted_array[:topk]
-        index, value = zip(*topk_array)
-        batch_value.append(value)
-        batch_index.append(index)
-    return np.array(batch_value), np.array(batch_index)
-
-
-def batch_select(data, index):
-    """bathc operation to sorted_logits[:, :top_p_num]"""
-    output = []
-    for i in range(data.shape[0]):
-        res = data[i, :index[i]]
-        output.append(res.reshape(1, -1))
-    return np.concatenate(output, 0)
-
-
-def sampler(log_probs_revised, top_p, top_k, use_pynative=False):
-    """Convert the log_probs to probability"""
-    if use_pynative:
-        logits = P.Pow()(np.e, Tensor(log_probs_revised, mstype.float32))
-    else:
-        logits = np.power(np.e, np.array(log_probs_revised, np.float32))
-
-    # If top_p is less than 1.0, use top_p sampling
-    if top_p < 1.0:
-        # Only consider the 5000 largest logits to reduce computation
-        if use_pynative:
-            sorted_logits, index = P.TopK(sorted=True)(logits, 5000)
-            cumsum_logits = P.CumSum()(sorted_logits, 1)
-            cumsum_logits = cumsum_logits.asnumpy()
-            index = index.asnumpy()
-            sorted_logits = sorted_logits.asnumpy()
-        else:
-            sorted_logits, index = topk_fun(logits, 5000)
-            cumsum_logits = np.cumsum(sorted_logits, 1)
-        cumsum_logits = cumsum_logits
-        index = index
-        sorted_logits = sorted_logits
-        top_p_num = np.sum(cumsum_logits < top_p, axis=-1) + 1
-        # Get the corresponding probs and indices
-        probs = batch_select(sorted_logits, top_p_num)
-        p_args = batch_select(index, top_p_num)
-        p = probs / np.sum(probs, -1, keepdims=True)
-        # if top_p is set to 1.0, use top_k sampling
-    else:
-        # Get the corresponding probs and indices
-        if use_pynative:
-            probs, p_args = P.TopK(sorted=True)(logits, top_k)
-            probs = probs.asnumpy()
-            p_args = p_args.asnumpy()
-        else:
-            probs, p_args = topk_fun(logits, top_k)
-        probs = probs
-        p_args = p_args
-        # Avoid rounding error
-        for i in range(probs.shape[0]):
-            if np.sum(probs[i]) == 0:
-                probs[i] = np.array([1 / top_k for _ in range(top_k)])
-        p = probs / np.sum(probs, -1, keepdims=True)
-    return p, p_args
 
 
 class GeneratorMixin:
@@ -158,6 +92,67 @@ class GeneratorMixin:
         outputs = P.LogSoftmax(-1)(logits)
         outputs = F.tensor_pow(np.e, outputs)
         return outputs
+
+    def _get_logits_processor(self,
+                              generation_config: GenerationConfig,
+                              logits_processor: Optional[LogitsProcessorList]):
+        """
+        This class returns a [`LogitsProcessorList`] list object that contains all relevant [`LogitsProcessor`]
+        instances used to modify the scores of the language model head.
+        """
+        # instantiate processors list
+        processors = LogitsProcessorList()
+
+        if generation_config.repetition_penalty is not None and generation_config.repetition_penalty != 1.0:
+            processors.append(RepetitionPenaltyLogitsProcessor(penalty=generation_config.repetition_penalty))
+        processors = self._merge_processor_list(processors, logits_processor)
+        # `LogitNormalization` should always be the last logit processor, when present
+        if generation_config.renormalize_logits is True:
+            processors.append(LogitNormalization())
+        return processors
+
+    def _merge_processor_list(self,
+                              default_list: LogitsProcessorList,
+                              custom_list: LogitsProcessorList):
+        """merge custom processor list with default list."""
+        if not custom_list:
+            return default_list
+        for default in default_list:
+            for custom in custom_list:
+                if type(custom) is type(default):
+                    object_type = "logits processor"
+                    raise ValueError(
+                        f"A custom {object_type} of type {type(custom)} with values {custom} has been passed to"
+                        f" `.generate()`, but it has already been created with the values {default}."
+                        f" {default} has been created by passing the corresponding arguments to generate or"
+                        f" by the model's config default values. If you just want to change the default values"
+                        f" of {object_type} consider passing them as arguments to `.generate()`"
+                        f" instead of using a custom {object_type}."
+                    )
+        default_list.extend(custom_list)
+        return default_list
+
+    def _get_logits_warper(self, generation_config: GenerationConfig):
+        """
+        This class returns a [`LogitsProcessorList`] list object that contains all relevant [`LogitsWarper`] instances
+        used for multinomial sampling.
+        """
+
+        # instantiate warpers list
+        warpers = LogitsProcessorList()
+
+        # all samplers can be found in `generation_utils_samplers.py`
+        if generation_config.temperature is not None and generation_config.temperature != 1.0:
+            warpers.append(TemperatureLogitsWarper(generation_config.temperature))
+        min_tokens_to_keep = 1
+        if generation_config.top_k is not None and generation_config.top_k != 0:
+            warpers.append(TopKLogitsWarper(top_k=generation_config.top_k, min_tokens_to_keep=min_tokens_to_keep))
+        if generation_config.top_p is not None and generation_config.top_p < 1.0:
+            warpers.append(TopPLogitsWarper(top_p=generation_config.top_p, min_tokens_to_keep=min_tokens_to_keep))
+        # `LogitNormalization` should always be the last logit processor, when present
+        if generation_config.renormalize_logits is True:
+            warpers.append(LogitNormalization())
+        return warpers
 
     def _prepare_model_inputs_for_decoder(self, input_ids, input_mask):
         """generate the inputs for the decoder"""
@@ -225,6 +220,8 @@ class GeneratorMixin:
     def _forward(self,
                  origin_inputs,
                  generation_config: GenerationConfig,
+                 logits_processor: Optional[LogitsProcessorList] = None,
+                 logits_warper: Optional[LogitsProcessorList] = None,
                  streamer: BaseStreamer = None,
                  **model_kwargs):
         """
@@ -242,7 +239,6 @@ class GeneratorMixin:
         """
         if generation_config.pad_token_id is None:
             generation_config.pad_token_id = 0
-        use_pynative = True
 
         if streamer is not None:
             streamer.put(origin_inputs[0])
@@ -270,8 +266,6 @@ class GeneratorMixin:
             else generation_config.max_length
         )
         logger.debug("max target_length is: %s", target_length)
-        # A list of the frequency of each token
-        frequency_list = None
         input_ids = self._pad_inputs_using_max_length(
             origin_inputs=origin_inputs, pad_token_id=generation_config.pad_token_id
         )
@@ -320,18 +314,15 @@ class GeneratorMixin:
                     valid_length_each_example[i] - 1 + i * seq_length
                     for i in range(batch_size)
                 ]
-                # current_index = Tensor(valid_length_each_example - 1, mstype.int32)
-                current_index = Tensor(current_index, mstype.int32)
                 logger.debug("validate length: %s", valid_length_each_example)
                 # pylint: disable=E1102
-                logits = self(
+                res = self(
                     input_ids=None,
                     attention_mask=encoder_mask,
                     encoder_outputs=encoder_output,
                     decoder_input_ids=inputs,
                     decoder_attention_mask=Tensor(target_mask, mstype.float32),
                 )
-                log_probs = self.process_logits(logits, current_index)
             else:
                 # model prepare input dict
                 model_inputs = self.prepare_inputs_for_generation( # pylint: disable=E1111
@@ -356,51 +347,44 @@ class GeneratorMixin:
                 # auto-aggressive generate
                 else:
                     res = self(**model_inputs)  # pylint: disable=E1102
-                if self.config.is_sample_acceleration:
-                    p, p_args = res
-                    if isinstance(p, Tensor):
-                        p = p.asnumpy()
-                    if isinstance(p_args, Tensor):
-                        p_args = p_args.asnumpy()
-                else:
-                    logits = res[0]
-                    log_probs = self.process_logits(
-                        logits, Tensor(current_index, mstype.int32), keep_all
-                    )
 
-            # Sample
+            # post process logits; skip this phase if post process is done in graph
             if not self.config.is_sample_acceleration:
-                log_probs = log_probs.asnumpy()
-                vocab_size = log_probs.shape[-1]
-                if generation_config.repetition_penalty != 1 and frequency_list is None:
-                    frequency_list = np.array([[0 for _ in range(vocab_size)]])
-                log_probs_revised = log_probs.reshape(batch_size, vocab_size)
-                if generation_config.repetition_penalty != 1:
-                    log_probs_revised = (
-                        log_probs
-                        - frequency_list * generation_config.repetition_penalty
-                        - (frequency_list > 0) * generation_config.repetition_penalty
-                    )
-                p, p_args = sampler(
-                    log_probs_revised,
-                    generation_config.top_p,
-                    generation_config.top_k,
-                    use_pynative,
-                )
+                # convert to numpy for post process
+                logits = res[0] if isinstance(res, tuple) else res
+                if isinstance(logits, Tensor):
+                    logits = logits.asnumpy().astype(np.float32)
+                logits = np.reshape(logits, (-1, logits.shape[-1]))
+                # need gather last seq logits using current_index
+                if not keep_all:
+                    logits = logits[current_index]
+
+                # post process logits, without changing logits shape and order
+                probs = logits_processor(input_ids, logits)
+                probs = logits_warper(input_ids, probs)
+                p_args = np.tile(np.arange(logits.shape[-1]), (batch_size, 1))
+            else:
+                probs, p_args = res
+                if isinstance(probs, Tensor):
+                    probs = probs.asnumpy().astype(np.float32)
+                if isinstance(p_args, Tensor):
+                    p_args = p_args.asnumpy()
 
             # Random select a token as final output for this round
             for i in range(batch_size):
                 if is_finished[i]:
                     continue
-                p_norm = softmax(p[i])
-                target_index = np.random.choice(len(p[i]), p=p_norm)
+                if generation_config.do_sample:
+                    # multinomial sample
+                    p_norm = softmax(probs[i])
+                    target_index = np.random.choice(len(probs[i]), p=p_norm)
+                else:
+                    # greedy
+                    target_index = np.argmax(probs[i])
 
-                # update frequency list
+                # get target token id
                 target = p_args[i][target_index]
-
-                if generation_config.repetition_penalty != 1:
-                    frequency_list[0][target] = frequency_list[0][target] + 1
-                input_ids[i, valid_length_each_example[i]] = p_args[i, target_index]
+                input_ids[i, valid_length_each_example[i]] = target
 
                 if streamer is not None:
                     streamer.put(np.asarray([target]))
@@ -431,6 +415,7 @@ class GeneratorMixin:
     def generate(self,
                  input_ids: Optional[Union[List[int], List[List[int]]]],
                  generation_config: Optional[GenerationConfig] = None,
+                 logits_processor: Optional[LogitsProcessorList] = None,
                  streamer: Optional[BaseStreamer] = None,
                  seed: Optional[int] = None,
                  **kwargs):
@@ -453,6 +438,10 @@ class GeneratorMixin:
                 from the model configuration will be used. Please note that unspecified parameters
                 will inherit [`GenerationConfig`]'s default values, whose documentation should be
                 checked to parameterize generation.
+            logits_processor (`LogitsProcessorList`, *optional*):
+                Custom logits processors that complement the default logits processors built from arguments and
+                generation config. If a logit processor is passed that is already created with the arguments or a
+                generation config an error is thrown. This feature is intended for advanced users.
             streamer: The streamer that generator uses.
             seed: Random seed used in sample.
             kwargs:
@@ -509,6 +498,8 @@ class GeneratorMixin:
         seed = 0 if seed is None else seed
         np.random.seed(seed)
 
+        logits_processor = logits_processor if logits_processor is not None else LogitsProcessorList()
+
         # Handle `generation_config` and kwargs that might update it
         # priority: `generation_config` argument > `model.generation_config` (default config)
         if generation_config is None:
@@ -521,13 +512,21 @@ class GeneratorMixin:
         )  # All unused kwargs must be model kwargs
 
         if not generation_config.do_sample:
-            generation_config.top_p = 1
-            generation_config.top_k = 1
+            generation_config.top_p = 1.0
+            generation_config.top_k = 0
         logger.debug("Generation Config is: %s", generation_config)
+
+        logits_processor = self._get_logits_processor(
+            generation_config=generation_config,
+            logits_processor=logits_processor,
+        )
+        logits_warper = self._get_logits_warper(generation_config)
 
         output_ids = self._forward(
             origin_inputs=input_ids,
             generation_config=generation_config,
+            logits_processor=logits_processor,
+            logits_warper=logits_warper,
             streamer=streamer,
             **model_kwargs,
         )
