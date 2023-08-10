4bf72b67c8410d21140f6536c89ee1a1fb2d0a85
Chenhua Geng <xwkgch@gmail.com>
2023-08-10 13:06:00 +0800

    bugfix

--- mindformers/core/callback/callback.py
+++ mindformers/core/callback/callback.py
@@ -400,7 +400,7 @@ class CheckpointMointor(ModelCheckpoint):
             self._last_time_for_keep = time.time()
             self._last_triggered_step = cb_params.cur_step_num
 
-            if context.get_context("enable_ge"):
+            if context.get_context("enable_ge") and os.getenv("MS_ENABLE_REF_MODE", "0") == "0":
                 set_cur_net(cb_params.train_network)
                 cb_params.train_network.exec_checkpoint_graph()
             if "epoch_num" in self._append_dict:
