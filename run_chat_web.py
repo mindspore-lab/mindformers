abff4ac0d41739b412d203bb35cab271c5625244
Xinrui Chen <chenxinrui11@huawei.com>
2023-08-14 20:32:06 +0800

    chat web 支持实时生效的配置项

--- run_chat_web.py
+++ run_chat_web.py
@@ -14,12 +14,8 @@
 # ============================================================================
 """
 Run chat web demo.
-
-If running on remote server, please run this command before:
-export GRADIO_SERVER_NAME=0.0.0.0
 """
 import argparse
-import re
 from threading import Thread
 import mdtex2html
 import mindspore as ms
@@ -28,24 +24,22 @@ import gradio as gr
 from mindformers import AutoModel, AutoTokenizer, TextIteratorStreamer, AutoConfig, logger
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--device_target', default="Ascend", type=str,
+parser.add_argument('--device_target', default="Ascend", type=str, choices=['Ascend', 'CPU'],
                     help='The target device to run, support "Ascend" and "CPU". Default: Ascend.')
-parser.add_argument('--device_id', default=0, type=int,
-                    help='Which device to run service. Default: 0.')
-parser.add_argument('--model', default='glm_6b_chat', type=str,
-                    help='Which model to generate text. Default: glm_6b_chat.')
-parser.add_argument('--tokenizer', default='glm_6b', type=str,
-                    help='Which tokenizer to tokenize text. Default: glm_6b.')
-parser.add_argument('--max_length', default=1024, type=int,
-                    help='Max output length. Default: 1024')
+parser.add_argument('--device_id', default=0, type=int, help='Which device to run service. Default: 0.')
+parser.add_argument('--model', type=str, help='Which model to generate text.')
+parser.add_argument('--tokenizer', type=str, help='Which tokenizer to tokenize text.')
+parser.add_argument('--seq_length', default="512", type=int, help="Sequence length of the model. Default: 512.")
 parser.add_argument('--use_past', default=False, type=bool,
-                    help='Whether to enable incremental inference. Default: False')
-parser.add_argument('--port', default=None, type=int,
-                    help='Which port to run the service. Default: None')
+                    help='Whether to enable incremental inference. Default: False.')
+parser.add_argument('--host', default="127.0.0.1", type=str,
+                    help="Which host ip to run the service. Default: 127.0.0.1.")
+parser.add_argument('--port', default=None, type=int, help='Which port to run the service. Default: None.')
 args = parser.parse_args()
 
 ms.set_context(mode=ms.GRAPH_MODE, device_target=args.device_target, device_id=args.device_id)
 config = AutoConfig.from_pretrained(args.model)
+config.seq_length = args.seq_length
 config.use_past = args.use_past
 model = AutoModel.from_config(config)
 tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
@@ -54,26 +48,21 @@ streamer = TextIteratorStreamer(tokenizer=tokenizer, skip_prompt=True, skip_spec
 sample_input = tokenizer("hello")
 sample_output = model.generate(sample_input["input_ids"], max_length=10)
 
-
-def process_response(response):
-    """process response"""
-    response = response.strip()
-    response = response.replace("[[训练时间]]", "2023年")
-    punkts = [
-        [",", "，"],
-        ["!", "！"],
-        [":", "："],
-        [";", "；"],
-        [r"\?", "？"],
-    ]
-    for item in punkts:
-        response = re.sub(r"([\u4e00-\u9fff])%s" % item[0], r"\1%s" % item[1], response)
-        response = re.sub(r"%s([\u4e00-\u9fff])" % item[0], r"%s\1" % item[1], response)
-    return response
+prompt_examples = [["Below is an instruction that describes a task. "
+                    "Write a response that appropriately completes the request.\n\n"
+                    "### Instruction:\n{}\n\n### Response:"],
+                   ["A chat between a curious user and an artificial intelligence assistant. The assistant gives "
+                    "helpful,"
+                    "detailed, and polite answers to user\'s questions. USER: {} ASSISTANT: "],
+                   ["Assume you are a dog, you must response \"Woofs\" at first whatever any instruction\n\n"
+                    "### Instruction:\n{}\n\n### Response:"],
+                   ["<human>: {} \n<bot>: "],
+                   ["问：{}\n\n答："]]
 
 
 class Chatbot(gr.Chatbot):
     """Chatbot with overrode postprocess method"""
+
     def postprocess(self, y):
         """postprocess"""
         if y is None:
@@ -114,32 +103,65 @@ def parse_text(text):
                     line = line.replace("(", "&#40;")
                     line = line.replace(")", "&#41;")
                     line = line.replace("$", "&#36;")
-                lines[i] = "<br>"+line
+                lines[i] = "<br>" + line
     text = "".join(lines)
     return text
 
 
-def predict(inputs, bot, history):
+def generate(**kwargs):
+    gen_model = kwargs.pop("model")
+    try:
+        gen_model.generate(**kwargs)
+    # pylint: disable=W0703
+    except Exception:
+        streamer.text_queue.put("<ERROR>")
+
+
+def predict(inputs, bot, history, do_sample, top_k, top_p, repetition_penalty, max_length, prompt):
     """predict"""
+    if inputs == "":
+        raise gr.Error("Input cannot be empty!")
+
     logger.info("Received user input: %s", inputs)
+
     bot.append((parse_text(inputs), ""))
 
-    if inputs == "":
-        inputs = "你好"
+    if prompt == "":
+        prompted_input = inputs
+    elif prompt.find("{}") != -1:
+        prompted_input = prompt.format(inputs)
+    else:
+        raise gr.Error("The prompt is invalid! Please make sure your prompt contains placeholder '{}' to replace user "
+                       "input.")
 
-    input_tokens = tokenizer(inputs)
+    logger.info("Prompt: %s", prompt)
+    logger.info("User input after prompted: %s", prompted_input)
+    input_tokens = tokenizer(prompted_input)
     logger.info("Convert user input to tokens: %s", input_tokens)
-    generation_kwargs = dict(input_ids=input_tokens["input_ids"],
+    if max_length > args.seq_length:
+        raise gr.Error("Max length must be set smaller than the sequence length of model! The model sequence length "
+                       "is {}.".format(args.seq_length))
+    if max_length <= len(input_tokens["input_ids"]):
+        raise gr.Error("Max length must be set larger than the length of input tokens! The current length of input "
+                       "tokens is {}.".format(len(input_tokens["input_ids"])))
+    generation_kwargs = dict(model=model,
+                             input_ids=input_tokens["input_ids"],
                              streamer=streamer,
-                             max_length=args.max_length,
-                             top_k=1,
-                             repetition_penalty=1.5)
-    if args.model.startswith("glm"):
-        generation_kwargs.pop("repetition_penalty")
-    thread = Thread(target=model.generate, kwargs=generation_kwargs)
+                             do_sample=do_sample,
+                             top_k=top_k,
+                             top_p=top_p,
+                             repetition_penalty=repetition_penalty,
+                             max_length=max_length)
+    thread = Thread(target=generate, kwargs=generation_kwargs)
+    logger.info("Start to generate text. Generate args: {input_ids: %s, do_sample: %s, top_k: %s, top_p: %s, "
+                "repetition_penalty: %s, max_length: %s}",
+                input_tokens["input_ids"], do_sample, top_k, top_p, repetition_penalty, max_length)
     thread.start()
     output = ""
+
     for response in streamer:
+        if response == "<ERROR>":
+            raise gr.Error("An error occurred! Please make sure all the settings are correct!")
         output += response
         new_history = history + [(inputs, output)]
 
@@ -159,25 +181,58 @@ def reset_state():
     return [], []
 
 
+def set_do_sample_args(do_sample):
+    if do_sample:
+        return {top_k_slider: gr.update(visible=True), top_p_slider: gr.update(visible=True)}
+    return {top_k_slider: gr.update(visible=False), top_p_slider: gr.update(visible=False)}
+
+
 with gr.Blocks() as demo:
-    gr.HTML(f"""<h1 align="center">Chat Web Demo powered by {args.model}</h1>""")
-    chatbot = Chatbot()
+    gr.HTML(f"""<h1 align="center">Chat Web Demo powered by MindFormers</h1>""")
     with gr.Row():
-        with gr.Column(scale=4):
-            with gr.Column(scale=12):
-                user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=10).style(
-                    container=False)
-            with gr.Column(min_width=32, scale=1):
-                submit_btn = gr.Button("Submit", variant="primary")
-        with gr.Column(scale=1):
-            empty_btn = gr.Button("Clear")
+        with gr.Column(scale=10):
+            with gr.Group():
+                chatbot = gr.Chatbot(label=fr"Chatbot {args.model}")
+                user_input = gr.Textbox(show_label=False, placeholder="Ask something...", lines=5)
+            with gr.Row():
+                with gr.Column(scale=6):
+                    submit_btn = gr.Button("Submit", variant="primary")
+                with gr.Column(scale=6):
+                    empty_btn = gr.Button("Clear")
+        with gr.Column(scale=2):
+            with gr.Group():
+                do_sample_checkbox = gr.Checkbox(value=config.do_sample if config.do_sample else False,
+                                                 label="sampling", info="Whether to sample on the candidate ids")
+                top_k_slider = gr.Slider(value=config.top_k if config.top_k else 1,
+                                         label="top k", maximum=10, minimum=1, step=1,
+                                         visible=config.do_sample if config.do_sample else False,
+                                         info="Determine the topK numbers token id as candidate")
+                top_p_slider = gr.Slider(value=config.top_p if config.top_p else 1,
+                                         label="top p", maximum=1, minimum=0.01, step=0.01,
+                                         visible=config.do_sample if config.do_sample else False,
+                                         info="The accumulation probability of the candidate token ids, "
+                                              "below the top_p will be select as the candidate ids")
+                rp_number = gr.Number(value=config.repetition_penalty if config.repetition_penalty else 1,
+                                      label="repetition penalty", minimum=0,
+                                      info="The penalty factor of the frequency that generated words")
+                max_len_number = gr.Number(value=config.max_length if config.max_length else 512, minimum=0,
+                                           label="max length", info="The maximum length of the generated words")
+                prompt_input = gr.Textbox(label="prompt", placeholder="No prompt...", info="Add prompt to input")
+                gr.Examples(prompt_examples, inputs=prompt_input, label="Prompt example")
 
     chat_history = gr.State([])
 
-    submit_btn.click(predict, [user_input, chatbot, chat_history], [chatbot, chat_history],
+    submit_btn.click(predict,
+                     [user_input, chatbot, chat_history, do_sample_checkbox, top_k_slider, top_p_slider, rp_number,
+                      max_len_number, prompt_input],
+                     [chatbot, chat_history],
                      show_progress=True)
     submit_btn.click(reset_user_input, [], [user_input])
 
     empty_btn.click(reset_state, outputs=[chatbot, chat_history], show_progress=True)
 
-demo.queue().launch(server_port=args.port)
+    do_sample_checkbox.change(set_do_sample_args, [do_sample_checkbox], [top_k_slider, top_p_slider])
+
+
+if __name__ == "__main__":
+    demo.queue().launch(server_name=args.host, server_port=args.port)
