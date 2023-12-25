dea5a8efb09344b607f7d09ebfec597d2c11de5c
xinlianglalala <huangxinliang4@huawei.com>
2023-12-25 19:32:10 +0800

    baichuan bugfix

--- research/baichuan2/baichuan2_13b.py
+++ research/baichuan2/baichuan2_13b.py
@@ -160,15 +160,16 @@ class Baichuan13BV2ForCausalLM(BaseModel):
                 label_mask = self.cast(self.not_equal(labels, self.ignore_token_id), mstype.float32)
                 input_mask = self.mul(input_mask, label_mask)
 
-        logits = self.cast(logits, mstype.float32)
         if not self.training:
             logits = self.reshape(logits, (bsz, seqlen, -1))
+            logits = self.cast(logits, mstype.float32)
             # makes cast effective to avoid allgather issue in Mindspore1.10
             input_mask = self.add(input_mask, 1)
             return logits, tokens, input_mask
 
         if logits.ndim > 2:
             logits = self.reshape(logits, (-1, logits.shape[-1]))
+        logits = self.cast(logits, mstype.float32)
         labels = self.reshape(labels, (-1,))
         input_mask = self.reshape(input_mask, (-1,))
         loss = self.loss(logits, labels, input_mask)
