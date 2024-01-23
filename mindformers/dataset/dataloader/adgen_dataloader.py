6fcf3a0e37cf18a3f328bb7d5de3f037e0102475
JoeyLin <joey.linxin@huawei.com>
2024-01-23 06:09:21 +0000

    !2177 修复adgen_dataloader的一个bug * fix adgen dataloader bug

--- mindformers/dataset/dataloader/adgen_dataloader.py
+++ mindformers/dataset/dataloader/adgen_dataloader.py
@@ -142,9 +142,9 @@ class ADGenDataset:
 
             examples[self.prompt_column] = content_list
             examples[self.response_column] = summary_list
-        assert self.__len__() > 0, f"valid data less then 1, loading data failed, please check your data file."
-        logger.info("Loading %d data success.", i)
         self.examples = examples
+        assert self.__len__() > 0, f"valid data less then 1, loading data failed, please check your data file."
+        logger.info("Loading %d data success.", self.__len__())
 
     def __len__(self):
         """Get the size of dataset"""
