f1961a4ac28f70bb0a16a85df1a55f4f0862ad23
wuzhiyuan1996 <wuzhiyuan15@huawei.com>
2024-07-03 00:34:16 +0800

    qwen适配fa小算子

--- research/qwen/qwen_model.py
+++ research/qwen/qwen_model.py
@@ -325,6 +325,9 @@ class QwenModel(QwenPreTrainedModel):
         mask = None
         if self.use_past:
             if self.is_first_iteration:
+                if not self.use_flash_attention:
+                    mask = self.casual_mask(input_ids)
+                    mask = self.casual_mask.post_process(mask)
                 if self.use_rope_self_define:
                     freqs_cis = self.freqs_mgr(seq_len)
                 else:
