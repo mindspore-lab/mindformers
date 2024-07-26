b176ed4245afc2ead373a3108f962debfa6af48d
杨星宇 <yangxingyu12@huawei.com>
2024-07-26 17:05:43 +0800

    [dev]1.将qwen1_5的st用例改成qwen22. 将qwen1的门禁从level1改0

--- tests/st/test_model/test_qwen_model/test_trainer.py
+++ tests/st/test_model/test_qwen_model/test_trainer.py
@@ -58,7 +58,7 @@ def generator_eval():
         yield train_data
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.platform_arm_ascend910b_training
 @pytest.mark.env_onecard
 class TestQwenTrainerMethod:
