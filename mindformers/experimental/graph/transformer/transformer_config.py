4ff04c46606b5427a038b23025d8f0e0bf6f9aba
zhangyihuiben <zhangyihuiben@sina.com>
2024-08-15 09:28:15 +0800

    add transformer_config.py, transformer_config_utils.py

--- mindformers/experimental/graph/transformer/transformer_config.py
+++ mindformers/experimental/graph/transformer/transformer_config.py
@@ -13,119 +13,250 @@
 # limitations under the License.
 # ============================================================================
 """Config Class"""
-from dataclasses import dataclass
+from typing import Callable
+
 import mindspore.common.dtype as mstype
 
+from mindformers.experimental.graph.transformer.utils import init_method_normal
 
-@dataclass
 class ModelParallelConfig:
+    r"""
+        Parallel config class for setting parallel configuration, such as the data parallel and model parallel.
+
+        Note:
+            Except the recompute argument, other arguments will **not** be effective when the user doesn't set
+            auto_parallel_context to `SEMI_AUTO_PARALLEL` or `AUTO_PARALLEL`.
+            The micro_batch_num must be greater than or equal to pipeline_stage when training.
+            The data_parallel\*model_parallel \*pipeline_stage must be equal or less equal to the device. When setting
+            the pipeline stage and optimizer_shard, the config will overwrite the auto_parallel_context. When given the
+            8 devices and the data_parallel is 1 and model_parallel is 1, the calculation will be repeated on each
+            device.
+
+        Args:
+            data_parallel (int): The data parallel way. The input data will be sliced into n parts for each layer
+                according to the data parallel way. Default: 1.
+            model_parallel (int): The model parallel way. The parameters of dense layers in MultiheadAttention and
+                FeedForward layer will be sliced according to the model parallel way. Default: 1.
+            context_parallel (int): The context parallel way. The context data will be sliced into n parts for each
+                layer according to the context parallel strategy. Default: 1.
+            vocab_emb_dp (bool): Shard embedding in model parallel or data parallel. If True, the embedding lookup
+                will be a data parallel style training and model_parallel value will be ignored.  If false, the
+                embedding table will be sharded into n parts at the 0-th dimension row slice of the embedding table,
+                where the n is the model parallel way determined by this parameter. Default: True
+
+        Returns:
+            Class, ModelParallelConfig.
     """
-    Base configuration.
 
-    The initialization function has an argument for each parameter.
-    """
-    data_parallel: int = 1
-    """The split number of data parallel."""
+    config_name = "model_parallel_config"
 
-    context_parallel: int = 1
-    """The split number of context parallel."""
+    def __init__(self,
+                 data_parallel: int = 1,
+                 tensor_parallel: int = 1,
+                 context_parallel: int = 1,
+                 vocab_emb_dp: bool = True,
+                 **kwargs):
+        super(ModelParallelConfig, self).__init__(**kwargs)
+        self.data_parallel = data_parallel
+        self.tensor_parallel = tensor_parallel
+        self.context_parallel = context_parallel
+        self.vocab_emb_dp = vocab_emb_dp
 
-    tensor_parallel: int = 1
-    """The split number of tensor parallel."""
 
-    vocab_emb_dp: bool = True
-    """Whether to split the vocabulary only along the dp dimension."""
-
-
-@dataclass
 class TransformerConfig(ModelParallelConfig):
+    r"""
+        Configuration object for mindformers transformers.
+
+        The initialization function has an argument for each parameter, including those in ModelParallelConfig.
+
+        Args:
+            batch_size (int): batch size for input data. Default: 1.
+            seq_length (int): The sequence length of input_ids. Default: 1.
+            padded_vocab_size (int): Vocabulary size of the model. Default: 1.
+            hidden_size (int): Dimensionality of the encoder layers and the pooler layer. Default: 1.
+            ffn_hidden_size (int): Transformer Feed-Forward Network hidden size. This is set to 4*hidden_size if not
+                provided.
+            num_layers (int): Number of hidden layers in the Transformer encoder. Default: 1.
+            num_attention_heads (int): Number of attention heads for each attention layer in the Transformer encoder.
+                Default: 1.
+            num_query_groups (int): Define multi group head attention heads number. Default: None.
+            max_position_embeddings (int): Sets the maximum position embedding size. Default: None.
+            layernorm_epsilon (float): Epsilon value for any LayerNorm operations. Default: 1e-5.
+            hidden_dropout (float): Dropout probability for transformer hidden state. Default: 0.0.
+            attention_dropout (float): Post attention dropout probability. Default: 0.0.
+            init_method_std (float): Standard deviation of the zero mean normal for the default initialization method,
+                not used if init_method and output_layer_init_method are provided. Default: 0.01.
+            rotary_percent (float): The proportion of the hidden dimension to which rotary positional embeddings will
+                be applied.Default: 1.0.
+            rotary_seq_len_interpolation_factor (float): scale of linearly interpolating RoPE for longer sequences.
+                The value must be a float larger than 1.0. Default: None.
+            compute_dtype (mstype): Linear layer compute dtype. Default: mstype.float32.
+            softmax_compute_dtype (mstype): Softmax compute dtype. Default: mstype.float32.
+            param_init_dtype (mstype): Parameter initial dtype. Default: mstype.float32.
+            add_bias_linear (bool): Include a bias term in all linear layers (QKV projections, after core attention,
+                and two in MLP layer). Default: False.
+            add_qkv_bias (bool): Add a bias term only for QKV projections. Default: False.
+            mlp_has_gate (bool): Apply gating in MLP block. Default: True.
+            gated_linear_unit (bool): Use a gated linear unit for the first linear layer in the MLP. Default: False.
+            fp16 (bool): If true, train with fp16 mixed precision training. Default: False.
+            bf16 (bool): If true, train with bf16 mixed precision training. Default: False.
+            group_query_attention (bool): Enable group query attention. Default: False.
+            use_flash_attn (bool): Whether enable flash attention ops. Default: False.
+            qkv_concat (bool): Whether to concatenate query, key, and value tensors. Default: False.
+            use_attn_mask_compression (bool): Whether to use attention mask compression. Default: False.
+            apply_query_key_layer_scaling (bool): If true, scale Q * K^T by 1 / layer-number. This improve numeric
+                stability when training with fp16. Default: False.
+            attention_softmax_in_fp32 (bool): If True, run attention masking and softmax in fp32. This should be True if
+                apply_query_key_layer_scaling is True. Default: True.
+            apply_residual_connection_post_layernorm (bool): If True, uses the original BERT residule connection
+                ordering. Default: False.
+            fp16_lm_cross_entropy (bool): If True, use FP16 precision for cross-entropy loss; otherwise, use FP32.
+                Default: False.
+            untie_embeddings_and_output_weights (bool): If True, the word embeddings and output layer weights are
+                untied; otherwise, they share the same weights. Default: True.
+            hidden_act (str): Specifies the activation function used in hidden layers. Default: "gelu".
+            mask_func_type (str): Attention mask compute method. Default: "attn_mask_fill".
+            normalization (str): Normalization used in transformerlayer block. Default: "RNSNorm".
+            position_embedding_type (str): Defines the type of position embedding to use. Default: "rope".
+            init_method (Callable): Method to initialize weights. Note that bias is always set to zero. Should be a
+                function that takes a single Tensor and initializes it. Default: None.
+            output_layer_init_method: Method to initialize weights of the output layer of both attention and MLP blocks.
+                If None, will be set to megatron.core.utils.scaled_init_method_normal(init_method_std) which is torch nn
+                init normal with mean=0.0 and std=init_method_std / math.sqrt(2.0 * num_layers). Default: None.
+
+        Returns:
+            Class, TransformerConfig.
     """
-    Configuration object for mindformers transformers.
 
-    The initialization function has an argument for each parameter, including those in ModelParallelConfig.
-    """
-    num_layers: int = 0
-    """Number of hidden layers in the Transformer encoder."""
+    config_name = "transformer_config"
 
-    hidden_size: int = 0
-    """Dimensionality of the encoder layers and the pooler layer."""
+    def __init__(self,
+                 batch_size: int = 1,
+                 seq_length: int = 1,
+                 padded_vocab_size: int = 1,
+                 hidden_size: int = 1,
+                 ffn_hidden_size: int = None,
+                 num_layers: int = 1,
+                 num_attention_heads: int = 1,
+                 num_query_groups: int = None,
+                 max_position_embeddings: int = None,
+                 layernorm_epsilon: float = 1e-5,
+                 hidden_dropout: float = 0.0,
+                 attention_dropout: float = 0.0,
+                 init_method_std: float = 0.01,
+                 rotary_percent: float = 1.0,
+                 rotary_seq_len_interpolation_factor: float = None,
+                 compute_dtype: mstype = mstype.float32,
+                 softmax_compute_dtype: mstype = mstype.float32,
+                 params_dtype: mstype = mstype.float32,
+                 add_bias_linear: bool = False,
+                 add_qkv_bias: bool = False,
+                 mlp_has_gate: bool = True,
+                 gated_linear_unit: bool = False,
+                 fp16: bool = False,
+                 bf16: bool = False,
+                 group_query_attention: bool = False,
+                 use_flash_attn: bool = False,
+                 qkv_concat: bool = False,
+                 use_attn_mask_compression: bool = False,
+                 apply_query_key_layer_scaling: bool = False,
+                 attention_softmax_in_fp32: bool = True,
+                 apply_residual_connection_post_layernorm: bool = False,
+                 fp16_lm_cross_entropy: bool = False,
+                 untie_embeddings_and_output_weights: bool = True,
+                 hidden_act: str = "gelu",
+                 mask_func_type: str = "attn_mask_fill",
+                 normalization: str = "RMSNorm",
+                 position_embedding_type: str = "rope",
+                 init_method: Callable = None,
+                 output_layer_init_method: Callable = None,
+                 **kwargs):
+        super(TransformerConfig, self).__init__(**kwargs)
+        self.batch_size = batch_size
+        self.seq_length = seq_length
+        self.padded_vocab_size = padded_vocab_size
+        self.hidden_size = hidden_size
+        self.ffn_hidden_size = ffn_hidden_size
+        self.num_layers = num_layers
+        self.num_attention_heads = num_attention_heads
+        self.num_query_groups = num_query_groups
+        self.max_position_embeddings = max_position_embeddings
+        self.layernorm_epsilon = layernorm_epsilon
+        self.hidden_dropout = hidden_dropout
+        self.attention_dropout = attention_dropout
+        self.init_method_std = init_method_std
+        self.rotary_percent = rotary_percent
+        self.rotary_seq_len_interpolation_factor = rotary_seq_len_interpolation_factor
+        self.compute_dtype = compute_dtype
+        self.softmax_compute_dtype = softmax_compute_dtype
+        self.params_dtype = params_dtype
+        self.add_bias_linear = add_bias_linear
+        self.add_qkv_bias = add_qkv_bias
+        self.mlp_has_gate = mlp_has_gate
+        self.gated_linear_unit = gated_linear_unit
+        self.fp16 = fp16
+        self.bf16 = bf16
+        self.group_query_attention = group_query_attention
+        self.use_flash_attn = use_flash_attn
+        self.qkv_concat = qkv_concat
+        self.use_attn_mask_compression = use_attn_mask_compression
+        self.apply_query_key_layer_scaling = apply_query_key_layer_scaling
+        self.attention_softmax_in_fp32 = attention_softmax_in_fp32
+        self.apply_residual_connection_post_layernorm = apply_residual_connection_post_layernorm
+        self.fp16_lm_cross_entropy = fp16_lm_cross_entropy
+        self.untie_embeddings_and_output_weights = untie_embeddings_and_output_weights
+        self.hidden_act = hidden_act
+        self.mask_func_type = mask_func_type
+        self.normalization = normalization
+        self.position_embedding_type = position_embedding_type
+        self.init_method = init_method
+        self.output_layer_init_method = output_layer_init_method
+        self.weight_init = "normal"  # TO DO: Delete
+        self.post_init_checks()
 
-    ffn_hidden_size: int = 0
-    """Transformer Feed-Forward Network hidden size."""
+    def post_init_checks(self):
+        """Modify attributes after initialization."""
+        if self.ffn_hidden_size is None:
+            self.ffn_hidden_size = 4 * self.hidden_size
 
-    num_attention_heads: int = 0
-    """Number of attention heads for each attention layer in the Transformer encoder."""
+        if self.fp16 and self.bf16:
+            raise ValueError(
+                f'Only one of self.fp16: {self.fp16} and self.bf16 {self.bf16} should be True.'
+            )
 
-    hidden_dropout: float = 0.1
-    """Dropout probability for transformer hidden state."""
+        if self.apply_query_key_layer_scaling:
+            self.attention_softmax_in_fp32 = True
 
-    hidden_act: str = 'gelu'
-    """Specifies the activation function used in hidden layers"""
+        if self.num_attention_heads % self.tensor_parallel != 0:
+            raise ValueError(
+                f"num_attention_heads ({self.num_attention_heads}) must be a multiple of "
+                f"tensor_model_parallel_size ({self.tensor_parallel})."
+            )
 
-    attention_dropout: float = 0.1
-    """Post attention dropout probability."""
+        if self.num_query_groups is None:
+            self.num_query_groups = self.num_attention_heads
 
-    layernorm_epsilon: float = 1e-5
-    """Epsilon value for any LayerNorm operations."""
+        if self.num_query_groups % self.tensor_parallel != 0:
+            raise ValueError(
+                f"num_query_groups ({self.num_query_groups}) must be a multiple of "
+                f"tensor_model_parallel_size ({self.tensor_parallel})."
+            )
 
-    add_bias_linear: bool = False
-    """Include a bias term in all linear layers (QKV projections, after core attention, and two in
-    MLP layer)."""
+        if self.max_position_embeddings is None:
+            self.max_position_embeddings = self.seq_length
 
-    add_qkv_bias: bool = False
-    """Add a bias term only for QKV projections."""
+        if self.init_method is None:
+            self.init_method = init_method_normal(self.init_method_std, self.params_dtype)
 
-    mlp_has_gate: bool = False
-    """Indicates whether the MLP layer includes a gating mechanism."""
+        self.init_method = None # TO DO: Delete
 
-    gated_linear_unit: bool = False
-    """Use a gated linear unit for the first linear layer in the MLP."""
+    def update(self):
+        """Modify attributes after covert."""
+        if self.ffn_hidden_size == 4:
+            self.ffn_hidden_size = 4 * self.hidden_size
 
-    compute_dtype: mstype = mstype.float32
-    """Linear layer compute dtype."""
+        if self.num_query_groups == 1:
+            self.num_query_groups = self.num_attention_heads
 
-    param_init_dtype: mstype = mstype.float32
-    """parameter initial dtype."""
-
-    softmax_compute_dtype: mstype = mstype.float32
-    """softmax compute dtype."""
-
-    fp16: bool = False
-    """If true, train with fp16 mixed precision training."""
-
-    bf16: bool = False
-    """If true, train with bf16 mixed precision training."""
-
-    mask_func_type: str = "attn_mask_fill"
-    """use type of masking function"""
-
-    group_query_attention: bool = False
-    """use Group Query Attention (GQA) mechanism."""
-
-    kv_num_heads: int = 0
-    """Define multi group head attention heads number."""
-
-    use_flash_attn: bool = False
-    """Whether enable flash attention ops."""
-
-    qkv_concat: bool = True
-    """whether to concatenate query, key, and value tensors."""
-
-    use_attn_mask_compression: bool = False
-    """whether to use attention mask compression."""
-
-    apply_residual_connection_post_layernorm: bool = True
-    """whether to apply residual connections after layer normalization."""
-
-    normalization: str = "RMSNorm"
-    """use type of normalization"""
-
-    init_method: callable = None
-    """Method to initialize weights. Note that bias is always set to zero. Should be a function that
-    takes a single Tensor and initializes it."""
-
-    weight_init: str = "normal"
-    """initialization method for weights."""
-
-    apply_query_key_layer_scaling: bool = False
-    """If true, scale Q * K^T by 1 / layer-number."""
+        if self.max_position_embeddings == 1:
+            self.max_position_embeddings = self.seq_length
---
+++ mindformers/experimental/graph/transformer/transformer_config_utils.py
@@ -0,0 +1,129 @@
+# Copyright 2024 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""TransformerConfig utils"""
+import mindspore.common.dtype as mstype
+
+from mindformers.experimental.graph.transformer.transformer_config import TransformerConfig
+from mindformers.models.configuration_utils import PretrainedConfig
+from mindformers.experimental.graph.transformer.utils import init_method_normal, init_method_zero
+
+_CONFIG_MAPPING = {
+    'vocab_size': 'padded_vocab_size',
+    'num_heads': 'num_attention_heads',
+    'n_kv_heads': 'num_query_groups',
+    'hidden_dropout_rate': 'hidden_dropout',
+    'attention_dropout_rate': 'attention_dropout',
+    'softmax_compute_type': 'softmax_compute_dtype',
+    'param_init_type': 'param_init_dtype',
+    'qkv_has_bias': 'add_qkv_bias',
+    'use_gqa': 'group_query_attention',
+    'use_flash_attention': 'use_flash_attn',
+    'max_position_embedding': 'max_position_embeddings',
+    'weight_init': 'init_method'
+}
+
+_INIT_ATTRIBUTE = {
+    '_name_or_path',
+    '_commit_hash',
+    'checkpoint_name_or_path',
+    'mindformers_version',
+    'tokenizer_class',
+    'architectures',
+    'is_encoder_decoder',
+    'is_sample_acceleration'
+}
+
+def convert_to_transformer_config(config, transformer_config):
+    r"""
+    Convert a configuration object to a TransformerConfig.
+
+    This function checks the type of the given configuration object and
+    converts it to the appropriate TransformerConfig using the relevant
+    conversion function.
+
+    Args:
+        config (Union[MindFormerConfig, PretrainedConfig]): The configuration object
+            to be converted. It should be an instance of either `MindFormerConfig`
+            or `PretrainedConfig`.
+
+        transformer_config (TransformerConfig): An existing TransformerConfig instance
+            to which the attributes from the provided config will be applied.
+
+    Returns:
+        TransformerConfig: The converted TransformerConfig object.
+
+    Raises:
+        Exception: If the provided config object is not of a supported type.
+
+    Examples:
+        >>> from mindformers.experimental.graph.transformer.transformer_config import TransformerConfig
+        >>> from mindformers.models.llama.llama_config import LlamaConfig\
+
+        >>> config = LlamaConfig()
+        >>> transformer_config = TransformerConfig()
+        >>> transformer_config = convert_to_transformer_config(config, transformer_config)
+    """
+    if isinstance(config, PretrainedConfig):
+        return convert_pretrained_config(config, transformer_config)
+
+    raise Exception(f"unsupported config type '{config}'.")
+
+def convert_pretrained_config(config: PretrainedConfig, transformer_config: TransformerConfig):
+    """Convert the PretrainedConfig class to TransformerConfig class."""
+    flag = 1
+    for attr in vars(config):
+        value = getattr(config, attr)
+        if attr.endswith('type'):
+            value = convert_mstype(value)
+        if attr == 'weight_init':
+            if str(value).lower() == 'zero' or str(value).lower() == 'zeros':
+                flag = 0
+                value = None
+            elif str(value).lower() == 'normal':
+                flag = 1
+                value = None
+        if attr in _CONFIG_MAPPING:
+            set_attr = _CONFIG_MAPPING[attr]
+            setattr(transformer_config, set_attr, value)
+        elif attr not in _INIT_ATTRIBUTE:
+            setattr(transformer_config, attr, value)
+
+    transformer_config.update()
+    transformer_config.post_init_checks()
+
+    transformer_config.data_parallel = config.parallel_config.data_parallel
+    transformer_config.tensor_parallel = config.parallel_config.model_parallel
+    transformer_config.context_parallel = config.parallel_config.context_parallel
+    transformer_config.vocab_emb_dp = config.parallel_config.vocab_emb_dp
+    if hasattr(config, 'n_kv_heads'):
+        if config.n_kv_heads is not None:
+            transformer_config.group_query_attention = True
+    if flag == 1:
+        transformer_config.init_method = init_method_normal(transformer_config.init_method_std,
+                                                            transformer_config.params_dtype)
+    else:
+        transformer_config.init_method = init_method_zero(transformer_config.params_dtype)
+    return transformer_config
+
+def convert_mstype(ms_dtype: str = "float16"):
+    """Convert the string type to MindSpore type."""
+    ms_type = str(ms_dtype).lower()
+    if ms_type == "float16":
+        return mstype.float16
+    if ms_type == "float32":
+        return mstype.float32
+    if ms_type == "bfloat16":
+        return mstype.bfloat16
+    return ms_dtype
