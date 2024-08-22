b45f1f9ff45fc81a07219863077dfd19ca749871
liyang <liyang360@huawei.com>
2024-08-22 11:46:15 +0800

    【Qwen2-7B】微调, 支持动态shape

---
+++ research/qwen2/alpaca_converter_json.py
@@ -0,0 +1,56 @@
+"""
+fastchat stanford alpaca data convert tools.
+"""
+import argparse
+import json
+
+import pathlib
+
+
+def main(data_path, output_path):
+    data_path = pathlib.Path(data_path)
+    with data_path.open() as f:
+        data = json.load(f)
+
+    sources = []
+    for example in data:
+        if example.get("input", "") == "":
+            sources.append(example['instruction'])
+        else:
+            instruction = example['instruction']
+            if instruction[-1] == ".":
+                instruction = instruction[:-1]
+            instruction = instruction + ": " + example['input']
+            sources.append(instruction)
+
+    targets = []
+    for example in data:
+        targets.append(example['output'])
+
+    new_data = []
+    for s, t in zip(sources, targets):
+        new_data.append({
+            "type": "chatml",
+            "conversations": [
+                {
+                    "from": "human",
+                    "value": s,
+                },
+                {
+                    "from": "gpt",
+                    "value": t,
+                },
+            ]
+        })
+
+    json.dump(new_data, open(output_path, "w"), ensure_ascii=False, indent=2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, default="alpaca-data.json")
+    parser.add_argument(
+        "--output_path", type=str, default="alpaca-data-conversation.json"
+    )
+    args = parser.parse_args()
+    main(args.data_path, args.output_path)
