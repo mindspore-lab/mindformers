90af8725ee59cb7cca259d407c1e059fe0b5aaa8
yinglailin <linyinglai@huawei.com>
2024-09-23 15:17:50 +0800

    "新增支持传入 args 运行模型"

--- tests/st/test_distri_core/test_config_from_cmd/test_config_from_cmd.py
+++ tests/st/test_distri_core/test_config_from_cmd/test_config_from_cmd.py
@@ -14,10 +14,12 @@
 # ============================================================================
 """test configs and initializations"""
 
+import argparse
+
 import pytest
 
 from mindformers.experimental.parallel_core.pynative.config import (
-    init_configs_from_cmd
+    init_configs_from_args
 )
 
 allowed_error = 1e-5
@@ -25,17 +27,26 @@ allowed_error = 1e-5
 
 @pytest.mark.level1
 @pytest.mark.platform_x86_cpu
-def test_config_from_cmd():
+def test_config_from_args():
     """
     Feature: init configs from command, as the basic test.
     Description: Test to initialize configs from command line
     Expectation: success
     """
-    base_cmd = "--seed 123 --lr 1e-2 --tensor-model-parallel-size 1 --num-layers 6 --train-iters 10 \
-                --data-path ./dataset --vocab-size 32000 --num-attention-heads 32 \
-                 --hidden-size 4096 --ffn-hidden-size 16384"
+
+    args = argparse.Namespace()
+    args.seed = 123
+    args.lr = 1e-2
+    args.tensor_model_parallel_size = 1
+    args.num_layers = 6
+    args.train_iters = 10
+    args.data_path = "./dataset"
+    args.padded_vocab_size = 32000
+    args.num_attention_heads = 32
+    args.hidden_size = 4096
+    args.ffn_hidden_size = 16384
     model_type = "model_config"
-    all_config = init_configs_from_cmd(base_cmd, model_type)
+    all_config = init_configs_from_args(args, model_type)
     assert all_config.training_config.seed == 123
     assert abs(all_config.optimizer_config.learning_rate - 1e-2) < allowed_error
     assert all_config.parallel_config.tensor_model_parallel_size == 1
