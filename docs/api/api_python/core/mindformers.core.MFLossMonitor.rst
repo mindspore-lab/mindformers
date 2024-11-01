0d0c47e01d364145751bbc00b946f7bd13acd913
zhangyihuiben <zhangyihuiben@sina.com>
2024-11-01 14:58:36 +0800

    tensorboard

--- docs/api/api_python/core/mindformers.core.MFLossMonitor.rst
+++ docs/api/api_python/core/mindformers.core.MFLossMonitor.rst
@@ -1,7 +1,7 @@
 mindformers.core.MFLossMonitor
 ==============================
 
-.. py:class:: mindformers.core.MFLossMonitor(learning_rate: Optional[Union[float, LearningRateSchedule]] = None, per_print_times: int = 1, micro_batch_num: int = 1, micro_batch_interleave_num: int = 1, origin_epochs: int = None, dataset_size: int = None, initial_epoch: int = 0, initial_step: int = 0, global_batch_size: int = 0, gradient_accumulation_steps: int = 1, enable_tensorboard: bool = False, tensorboard_path: str = None, check_for_nan_in_loss_and_grad: bool = False)
+.. py:class:: mindformers.core.MFLossMonitor(learning_rate: Optional[Union[float, LearningRateSchedule]] = None, per_print_times: int = 1, micro_batch_num: int = 1, micro_batch_interleave_num: int = 1, origin_epochs: int = None, dataset_size: int = None, initial_epoch: int = 0, initial_step: int = 0, global_batch_size: int = 0, gradient_accumulation_steps: int = 1, check_for_nan_in_loss_and_grad: bool = False)
 
     监控训练过程中loss等相关参数的回调函数。
 
@@ -16,6 +16,4 @@ mindformers.core.MFLossMonitor
         - **initial_step** (int) - 训练开始的step数。默认值： ``0`` 。
         - **global_batch_size** (int) - 总BatchSize大小。默认值： ``0`` 。
         - **gradient_accumulation_steps** (int) - 梯度累加步数。默认值： ``1`` 。
-        - **enable_tensorboard** (bool) - 是否在训练中启用 TensorBoard 日志记录。默认值： ``False`` 。
-        - **tensorboard_path** (str) - 如果启用了 TensorBoard，则该路径指定TensorBoard 日志文件保存的路径。如果设置为 None，则使用默认输出路径（例如 './output'）。默认值：None。
         - **check_for_nan_in_loss_and_grad** (bool) - 是否检查损失和梯度存在Nan。默认值： ``False`` 。
---
+++ mindformers/utils/tensorboard.py
@@ -0,0 +1,83 @@
+# Copyright 2024 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""TENSORBOARD Module"""
+from tensorboardX import SummaryWriter
+
+from mindformers.tools.logger import logger
+from mindformers.tools.register import MindFormerConfig
+
+_GLOBAL_TENSORBOARD_WRITER = None
+_GLOBAL_TENSORBOARD_WRITER_ARGS = {'log_timers_to_tensorboard': False,
+                                   'log_loss_scale_to_tensorboard': False
+                                   }
+
+
+def get_tensorboard_writer():
+    """Return tensorboard writer. It can be None so no need to check if it is initialized."""
+    return _GLOBAL_TENSORBOARD_WRITER
+
+
+def _set_tensorboard_writer(config):
+    """Set tensorboard writer."""
+    global _GLOBAL_TENSORBOARD_WRITER
+
+    if hasattr(config, 'tensorboard_dir') and config.tensorboard_dir:
+        if _GLOBAL_TENSORBOARD_WRITER is not None:
+            logger.warning("tensorboard writer is already initialized.")
+        else:
+            logger.info('......setting tensorboard......')
+            queue_size = getattr(config, 'tensorboard_queue_size', 10)
+            if not queue_size:
+                queue_size = 10
+            _GLOBAL_TENSORBOARD_WRITER = SummaryWriter(
+                log_dir=config.tensorboard_dir,
+                max_queue=queue_size)
+
+
+def _unset_tensorboard_writer():
+    """Unset tensorboard writer."""
+    global _GLOBAL_TENSORBOARD_WRITER
+    if _GLOBAL_TENSORBOARD_WRITER is not None:
+        if isinstance(_GLOBAL_TENSORBOARD_WRITER, SummaryWriter):
+            _GLOBAL_TENSORBOARD_WRITER.close()
+        else:
+            logger.warning("The global tensorboard writer is not an instance of SummaryWriter.")
+        _GLOBAL_TENSORBOARD_WRITER = None
+
+
+def write_args_to_tensorboard(config):
+    """Write arguments to tensorboard."""
+    global _GLOBAL_TENSORBOARD_WRITER
+    if _GLOBAL_TENSORBOARD_WRITER is None:
+        raise ValueError("tensorboard writer is not set.")
+    writer = get_tensorboard_writer()
+    if isinstance(config, MindFormerConfig):
+        for key, value in config.items():
+            writer.add_text(key, str(value), global_step=1)
+    else:
+        logger.warning("Only MindFormerConfig will be written.")
+
+
+def update_tensorboard_args(config):
+    """Update args for tensorboard writer."""
+    global _GLOBAL_TENSORBOARD_WRITER_ARGS
+    for key in _GLOBAL_TENSORBOARD_WRITER_ARGS:
+        if hasattr(config, key) and config.key is not None:
+            _GLOBAL_TENSORBOARD_WRITER_ARGS[key] = getattr(config, key)
+
+
+def get_tensorboard_args():
+    """Return args for tensorboard writer."""
+    return _GLOBAL_TENSORBOARD_WRITER_ARGS
