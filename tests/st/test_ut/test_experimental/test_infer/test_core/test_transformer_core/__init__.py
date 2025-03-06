6c243c7ff990876785a811787385fd469df3d719
pengjingyou <pengjingyou@huawei.com>
2025-03-06 22:39:28 +0800

    【dev】【MCore】新增TransformerBlock, TransformerLayer接口ut用例

--- tests/st/test_ut/test_experimental/test_infer/test_core/test_transformer_core/__init__.py
+++ tests/st/test_ut/test_experimental/test_infer/test_core/test_transformer_core/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""test infer rope."""
+"""test infer transformer core."""
