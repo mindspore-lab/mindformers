d8c72d6c1097efa7548780d64e35ff1d30bffa7c
zhangyihuiben <zhangyihuiben@sina.com>
2025-05-21 16:45:19 +0800

    CleanCode

--- research/telechat2/telechat_tokenizer.py
+++ research/telechat2/telechat_tokenizer.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 # Copyright 2024 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-# coding: utf-8
 """Telechat tokenizer APIs."""
 import os
 from shutil import copyfile
