8fcc17d324fdfd4dded8eaa4171abfee881de986
leicichen <736401803@qq.com>
2025-05-23 17:14:56 +0800

    [dev][mcore]:增加Flash_attention代码及其功能UT

---
+++ mindformers/parallel_core/inference/transformer/flash_attention.py
@@ -0,0 +1,170 @@
+# Copyright 2025 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Flash Attention Layer"""
+__all__ = ['FlashAttention']
+
+import mindspore.common.dtype as mstype
+from mindspore import ops
+from mindspore import Parameter
+from mindspore.common.initializer import initializer
+from mindspore.nn.cell import Cell
+from mindspore.ops.operations.nn_ops import FlashAttentionScore
+
+
+class FlashAttention(Cell):
+    """Flash Attention Layer.
+
+    This function contains the flash attention primitives used in FlashAttention (see paper) and PagedAttention.
+    `FlashAttention: Fast and Memory-Efficient Exact Attention with IO-Awareness <https://arxiv.org/pdf/2205.14135.pdf>`
+
+    Args：
+        - head_num (int): Number of attention heads.
+        - kv_cache_shape (tuple): Shape of the key-value cache tensor.
+        - head_dim (Optional[int]): Dimension of each attention head. Default: None.
+        - kv_head_num (Optional[int]): Number of key-value heads. Default: None
+        - keep_prob (float): Dropout keep probability. Default: 1.0.
+        - scale_value (float): Scaling factor for attention scores. Default: 1.0.
+        - pre_tokens (int): Number of previous tokens to consider. Default: 2147483647.
+        - next_tokens (int): Number of next tokens to consider. Default: 2147483647.
+        - sparse_mode (int): Mode for sparse attention. Default: 0.
+        - compute_dtype (mstype): Data type for computation. Default: mstype.float16.
+        - input_layout (str): Layout of input tensors("TH" or "TND"). Default: "TH".
+
+
+    Inputs:
+        - **query** (Tensor[float16, bfloat16]) - The query tensor.
+        - **key** (Tensor[float16, bfloat16]) - The key tensor.
+        - **value** (Tensor[float16, bfloat16]) - The value tensor.
+        - **kv_cache** (Tensor) - reserved field
+        - **slot_mapping** (Tensor) - Store token cache physical slot index.
+        - **block_tables** (Tensor) - The block mapping table with data type of int32.
+        - **batch_valid_length** (Tensor) -  In incremental inference, a tensor used for calculating the index
+          of the previous step. It is of int32 type and has a shape of [batch_size].
+        - **context_lens_tensor** (Tensor) - The context length of each sequence with data type of int32.
+        - **actual_seq_qlen** (Union[List[int64], Tuple[int64], None]) - Size of query corresponding to each batch,
+          array with increasing values and the last value equal to T1.
+        - **actual_seq_kvlen** (Union[List[int64], Tuple[int64], None]) - Size of key and value corresponding to each
+          batch, array with increasing values and the last value equal to T2.
+        - **attn_mask** (Union[Tensor, None]) - The attention mask tensor.
+        - **padding_mask** (None) - Reserved parameter. Not implemented yet.
+        - **prefix** (Union[Tensor[int64], None]) - N value of each Batch in the prefix sparse calculation scenario.
+          Not implemented yet. Input tensor of shape :math:`(B,)`.
+
+    Outputs:
+        - **attention_out** (Tensor[float16, bfloat16]) - The output of attention, its shape, and data type
+          are the same as the query.
+
+    Supported Platforms:
+        ``Ascend``
+    """
+
+    def __init__(
+            self,
+            head_num,
+            kv_cache_shape,
+            head_dim=None,
+            kv_head_num=None,
+            keep_prob=1.0,
+            scale_value=1.0,
+            pre_tokens=2147483647,
+            next_tokens=2147483647,
+            sparse_mode=0,
+            compute_dtype=mstype.float16,
+            input_layout="TH"
+    ):
+        super().__init__()
+        self.head_num = head_num
+        self.hidden_size_per_attention_head = head_dim
+        self.kv_head_num = kv_head_num
+        self.sparse_mode = sparse_mode
+        self.is_prefill = True
+        self.input_layout = input_layout
+
+        self.reshape_and_cache = ops.auto_generate.ReshapeAndCache()
+
+        self.flash_attention = FlashAttentionScore(
+            head_num=head_num,
+            keep_prob=keep_prob,
+            scale_value=scale_value,
+            pre_tokens=pre_tokens,
+            next_tokens=next_tokens,
+            inner_precise=0,
+            input_layout=self.input_layout,
+            sparse_mode=self.sparse_mode)
+
+        self.paged_attention = ops.auto_generate.PagedAttention(
+            self.head_num, scale_value, self.kv_head_num)
+
+        self.key_cache = Parameter(initializer('zeros', kv_cache_shape,
+                                               compute_dtype),
+                                   name="key_cache",
+                                   requires_grad=False)
+        self.value_cache = Parameter(initializer('zeros', kv_cache_shape,
+                                                 compute_dtype),
+                                     name="value_cache",
+                                     requires_grad=False)
+
+    # pylint: disable=W0613
+    def construct(self,
+                  query,
+                  key,
+                  value,
+                  kv_cache=None,
+                  slot_mapping=None,
+                  block_tables=None,
+                  batch_valid_length=None,
+                  context_lens_tensor=None,
+                  q_seq_lens=None,
+                  actual_seq_qlen=None,
+                  actual_seq_kvlen=None,
+                  attn_mask=None,
+                  padding_mask=None,
+                  prefix=None):
+        """Forward process of the FlashAttention."""
+
+        bs, seq_len, _ = query.shape
+        key_cache = self.key_cache
+        value_cache = self.value_cache
+        self.reshape_and_cache(key, value, self.key_cache, self.value_cache,
+                               slot_mapping)
+
+        if self.is_prefill:
+
+            if self.input_layout == "TH":
+                query = query.reshape((-1, self.head_num * self.hidden_size_per_attention_head))
+                key = key.reshape((-1, self.kv_head_num * self.hidden_size_per_attention_head))
+                value = value.reshape((-1, self.kv_head_num * self.hidden_size_per_attention_head))
+
+            if self.input_layout == "TND":
+                query = query.reshape((-1, self.head_num, self.hidden_size_per_attention_head))
+                key = key.reshape((-1, self.kv_head_num, self.hidden_size_per_attention_head))
+                value = value.reshape((-1, self.kv_head_num, self.hidden_size_per_attention_head))
+
+            _, _, _, output = self.flash_attention(query, key, value,
+                                                   None, None,
+                                                   padding_mask, attn_mask,
+                                                   prefix, actual_seq_qlen,
+                                                   actual_seq_kvlen)
+            context_layer = output
+        else:
+            context_layer = self.paged_attention(query, key_cache, value_cache,
+                                                 block_tables, batch_valid_length, None,
+                                                 None, attn_mask,
+                                                 q_seq_lens)
+
+        core_attn_out = context_layer.reshape(
+            (bs, seq_len, self.head_num * self.hidden_size_per_attention_head))
+
+        return core_attn_out
---
+++ tests/st/test_ut/test_parallel_core/test_inference/test_transformer/test_flash_attention/test_infer_flash_attention.py
@@ -0,0 +1,195 @@
+# Copyright 2025 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""mcore Flash_Attn UT of inference"""
+import subprocess
+from pathlib import Path
+import numpy as np
+import pytest
+
+from mindformers.tools.logger import logger
+from tests.st.test_ut.test_parallel_core.test_inference.test_transformer.test_flash_attention.data_gen_utils import (
+    get_init_params,
+    BATCH_SIZE,
+    SEQ_LENGTH,
+    NUM_HEADS,
+    HIDDEN_SIZE,
+)
+
+FA_SINGLE_CARD_TEST_PARAM = "model_args, data_keys, expect_error"
+FA_SINGLE_CARD_TEST_CASES = [
+    (
+        # 并行策略: 单卡, batch_size: 2, seq_length: 2, num_heads: 2,
+        # n_kv_heads: 1, hidden_size: 32, is_prefill: TRUE
+        # keep_prob: 1.0, scale_value: 0.25
+        # expected result: 功能跑通。
+        {"batch_size": BATCH_SIZE, "seq_length": SEQ_LENGTH, "num_heads": NUM_HEADS,
+         "n_kv_heads": 1, "hidden_size": HIDDEN_SIZE, "is_prefill": True,
+         "keep_prob": 1.0, "scale_value": 0.25},
+        {"output": "output_1"},
+        False
+    ),
+    (
+        # 并行策略: 单卡, batch_size: 2, seq_length: 2, num_heads: 2,
+        # n_kv_heads: 2, hidden_size: 32, is_prefill: TRUE
+        # keep_prob: 1.0, scale_value: 0.25
+        # expected result: 功能跑通。
+        {"batch_size": BATCH_SIZE, "seq_length": SEQ_LENGTH, "num_heads": NUM_HEADS,
+         "n_kv_heads": NUM_HEADS, "hidden_size": HIDDEN_SIZE, "is_prefill": True,
+         "keep_prob": 1.0, "scale_value": 0.25},
+        {"output": "output_2"},
+        False
+    ), (
+        # 并行策略: 单卡, batch_size: 2, seq_length: 2, num_heads: 2,
+        # n_kv_heads: 1, hidden_size: 32, is_prefill: FALSE
+        # keep_prob: 1.0, scale_value: 0.25
+        # expected result: 功能跑通。
+        {"batch_size": BATCH_SIZE, "seq_length": SEQ_LENGTH, "num_heads": NUM_HEADS,
+         "n_kv_heads": 1, "hidden_size": HIDDEN_SIZE, "is_prefill": False,
+         "keep_prob": 1.0, "scale_value": 0.25},
+        {"output": "output_3"},
+        False
+    ), (
+        # 并行策略: 单卡, batch_size: 2, seq_length: 2, num_heads: 2,
+        # n_kv_heads: 2, hidden_size: 32, is_prefill: FALSE
+        # keep_prob: 1.0, scale_value: 0.25
+        # expected result: 功能跑通。
+        {"batch_size": BATCH_SIZE, "seq_length": SEQ_LENGTH, "num_heads": NUM_HEADS,
+         "n_kv_heads": NUM_HEADS, "hidden_size": HIDDEN_SIZE, "is_prefill": False,
+         "keep_prob": 1.0, "scale_value": 0.25},
+        {"output": "output_4"},
+        False
+    ), (
+        # 并行策略: 单卡, batch_size: 2, seq_length: 2, num_heads: 2,
+        # n_kv_heads: 1, hidden_size: 32, is_prefill: TRUE
+        # keep_prob: 1.0, scale_value: 1.0
+        # expected result: 功能跑通。
+        {"batch_size": BATCH_SIZE, "seq_length": SEQ_LENGTH, "num_heads": NUM_HEADS,
+         "n_kv_heads": 1, "hidden_size": HIDDEN_SIZE, "is_prefill": True,
+         "keep_prob": 1.0, "scale_value": 1.0},
+        {"output": "output_5"},
+        False
+    )
+]
+
+
+def build_msrun_command_list(
+        run_script_path, batch_size,
+        seq_length, num_heads, n_kv_heads, hidden_size, is_prefill,
+        keep_prob, scale_value, output_path_param: str = None
+):
+    """ Build the msrun command with the specified parameters. """
+    cmd_list = [
+        "python",
+        str(run_script_path),
+        f"--batch_size={batch_size}",
+        f"--seq_length={seq_length}",
+        f"--num_heads={num_heads}",
+        f"--n_kv_heads={n_kv_heads}",
+        f"--hidden_size={hidden_size}",
+        f"--is_prefill={str(is_prefill).lower()}",
+        f"--keep_prob={keep_prob}",
+        f"--scale_value={scale_value}",
+        f"--output_path={output_path_param}",
+    ]
+    logger.info(f"Equivalent shell command for debugging (approximate): {' '.join(cmd_list)}")
+    return cmd_list
+
+
+class TestFlashAttention:
+    """Test class for Flash Attention with different configurations"""
+    OUTPUT_MS_FILENAME = "output_ms_fa.npz"
+    LOG_DIR_NAME = "msrun_log"
+    WORKER_LOG_FILENAME = "worker_0.log"
+
+    def setup_method(self):
+        """Setup method to prepare test environment"""
+        self.sh_path = Path(__file__).parent.resolve()
+        self.run_script_path = self.sh_path / "run_infer_flash_attention.py"
+
+    @staticmethod
+    def check_function(output_ms_dict, model_args, data_keys):
+        """
+        Compare the shapes of output_ms and input_ms whether they are the same.
+        """
+        for key, _ in data_keys.items():
+            output_data = output_ms_dict.get(key)
+            query = get_init_params(is_prefill=model_args["is_prefill"],
+                                    n_kv_heads=model_args["n_kv_heads"])["query"]
+
+            assert output_data.shape == query.shape, \
+                (f"The shapes of output data and input data are different, "
+                 f"got output shape: {output_data.shape} and input shape: {query.shape}")
+
+    def run_test(
+            self,
+            model_args,
+            data_keys,
+            tmp_path,
+            expect_error=False,
+    ):
+        """Helper function to run test"""
+        output_file_path = tmp_path / self.OUTPUT_MS_FILENAME
+        log_dir_path = tmp_path / self.LOG_DIR_NAME
+        log_dir_path.mkdir(parents=True, exist_ok=True)
+
+        cmd_list = build_msrun_command_list(
+            run_script_path=self.run_script_path,
+            batch_size=model_args["batch_size"],
+            seq_length=model_args["seq_length"],
+            num_heads=model_args["num_heads"],
+            n_kv_heads=model_args["n_kv_heads"],
+            hidden_size=model_args["hidden_size"],
+            is_prefill=model_args["is_prefill"],
+            keep_prob=model_args["keep_prob"],
+            scale_value=model_args["scale_value"],
+            output_path_param=output_file_path,
+        )
+
+        cmd_result = subprocess.run(
+            cmd_list, shell=False, capture_output=True, text=True, check=False)
+
+        if expect_error:
+            assert cmd_result.returncode != 0, (
+                f"Expected an error but test script passed. "
+                f"Stdout:\n{cmd_result.stdout}\n"
+                f"Stderr:\n{cmd_result.stderr}\n"
+            )
+        else:
+            assert cmd_result.returncode == 0, (
+                f"Test script failed with non-zero exit code: "
+                f"{cmd_result.returncode}.\nStdout:\n{cmd_result.stdout}\nStderr:\n{cmd_result.stderr}\n"
+            )
+            assert output_file_path.exists(), (
+                f"Output file {output_file_path} was not created."
+            )
+
+        ouput_ms_dict = np.load(output_file_path)
+
+        self.check_function(ouput_ms_dict, model_args, data_keys)
+
+    @pytest.mark.level0
+    @pytest.mark.platform_arm_ascend910b_training
+    @pytest.mark.env_onecard
+    @pytest.mark.parametrize(
+        FA_SINGLE_CARD_TEST_PARAM,
+        FA_SINGLE_CARD_TEST_CASES
+    )
+    def test_single_card_configurations(self, model_args, data_keys, expect_error, tmp_path):
+        """Test single card with various configurations."""
+        self.run_test(
+            model_args=model_args, expect_error=expect_error,
+            data_keys=data_keys,
+            tmp_path=tmp_path
+        )
