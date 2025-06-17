a1c7195d2697df6523b95501fcc17d4cff316e8d
zhanzhan1 <zhanzhan1@huawei.com>
2025-06-17 17:19:09 +0800

    bugfix for qwen generator

--- mindformers/experimental/infer/models/llama/llama.py
+++ mindformers/experimental/infer/models/llama/llama.py
@@ -19,7 +19,6 @@ from multiprocessing.synchronize import Condition
 from safetensors import safe_open
 import numpy as np
 
-import mindspore as ms
 import mindspore.common.dtype as mstype
 from mindspore import Tensor, ops, mint, mutable
 from mindspore.communication import get_group_size
@@ -126,11 +125,9 @@ class ParallelLlamaForCausalLM(LlamaPreTrainedModel):
             model_inputs = self._prepare_inputs_for_prefill_flatten(origin_inputs,
                                                                     batch_valid_length,
                                                                     slot_mapping,
-                                                                    model_inputs,
-                                                                    True)
-        model_inputs["batch_valid_length"] = Tensor.from_numpy(batch_valid_length.astype(np.int32))
-        position_ids = batch_valid_length - 1
-        model_inputs["position_ids"] = ms.Tensor(position_ids, dtype=ms.int32).reshape(-1)
+                                                                    model_inputs,)
+        position_ids = batch_valid_length.astype(np.int32) - 1
+        model_inputs["position_ids"] = Tensor.from_numpy(position_ids.reshape((-1,)))
 
         if not prefill:
             q_seq_lens = np.ones(batch_valid_length.shape, dtype=np.int32).reshape(-1)
@@ -139,6 +136,7 @@ class ParallelLlamaForCausalLM(LlamaPreTrainedModel):
         model_inputs["q_seq_lens"] = Tensor.from_numpy(q_seq_lens)
 
         model_inputs["attention_mask"] = self.model.casual_mask.gen_attention_mask(prefill)
+        model_inputs["need_flatten"] = True
         return model_inputs
 
     def set_dynamic_inputs(self, **kwargs):
