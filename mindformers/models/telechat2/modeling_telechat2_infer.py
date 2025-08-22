0e02cab7ceac817df6aad826963b72f115272b67
zhanzhan1 <zhanzhan1@huawei.com>
2025-08-22 17:11:42 +0800

    telechat support hidden_states input

--- mindformers/models/telechat2/modeling_telechat2_infer.py
+++ mindformers/models/telechat2/modeling_telechat2_infer.py
@@ -82,15 +82,16 @@ class InferenceTelechat2ForCausalLM(Telechat2PreTrainedModel, InferModelMixin):
                               model_comm_pgs=self.model_comm_pgs)
 
     @jit
-    def construct(self, input_ids, positions=None, batch_valid_length=None, context_lens_tensor=None, q_seq_lens=None,
-                  block_tables=None, slot_mapping=None, attention_mask=None, attn_metadata=None,
-                  key_cache=None, value_cache=None):
+    def construct(self, input_ids, hidden_states=None, positions=None, batch_valid_length=None,
+                  context_lens_tensor=None, q_seq_lens=None, block_tables=None, slot_mapping=None,
+                  attention_mask=None, attn_metadata=None, key_cache=None, value_cache=None):
         r"""
         model forward.
 
         Args:
             input_ids: input ids.
             positions: position ids.
+            hidden_states: hidden states.
             batch_valid_length: actual seq length.
             context_lens_tensor: computed key value length.
             block_tables: Store mapping tables for each sequence.
@@ -106,6 +107,7 @@ class InferenceTelechat2ForCausalLM(Telechat2PreTrainedModel, InferModelMixin):
         """
         logits = self.model(
             input_ids=input_ids,
+            hidden_states=hidden_states,
             positions=positions,
             batch_valid_length=batch_valid_length,
             context_lens_tensor=context_lens_tensor,
