fix: update loss calculation to handle zero auxiliary loss scale

mindformers/core/callback/callback.py
@@ -534,7 +534,7 @@ class MFLossMonitor(Callback):
             loss_info = "loss:[%5.6f/%5.6f], " % (loss, np.mean(self.loss_list))
         if self.print_separate_loss:
             separate_loss = "lm_loss: %5.6f, " % main_loss
-            if self.is_moe_model:
+            if self.is_moe_model and np.all(extra_loss > 0):
                 separate_loss += "load_balancing_loss: %5.6f, " % extra_loss
             if self.is_mtp_model:
                 separate_loss += "mtp_loss: %5.6f, " % mtp_loss

mindformers/wrapper/wrapper.py
@@ -404,7 +404,7 @@ class MFTrainOneStepCell(nn.TrainOneStepWithLossScaleCell):
             grad_scale_factor = self.grad_scale_factor
 
         if self.print_separate_loss:
-            F.assign_add(self.aux_loss_parameter, aux_loss / self.aux_loss_scale)
+            F.assign_add(self.aux_loss_parameter, aux_loss / self.aux_loss_scale if self.aux_loss_scale > 0 else 0)
             F.assign_add(self.mtp_loss_parameter, mtp_loss)
             F.assign_add(self.lm_loss_parameter, lm_loss)
         return loss, grads, grad_scale_factor